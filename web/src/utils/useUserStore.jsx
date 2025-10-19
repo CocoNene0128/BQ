1a8e2ef9e2dd78a58b7993a38e96ce4169cf5d09
CocoNene0128 <wasd9611456@gmail.com>
2025-10-19 21:14:56 +0900

    useUserStore.jsx

--- web/src/utils/useUserStore.jsx
+++ web/src/utils/useUserStore.jsx
@@ -1,9 +1,30 @@
-import {create} from "zustand/react";
+// import {create} from "zustand/react";
+//
+// const useUserStore = create((set) => ({
+//     user: null,
+//     setUser: (userData) => set({user: userData}),
+//     clearUser: () => set({user: null}),
+// }))
+//
+// export default useUserStore;
+
+
+
+// -----------------------------
+// 🧩 시연용: 로그인 없이도 가짜 유저 데이터로 초기화
+// -----------------------------
+import { create } from "zustand/react";
 
 const useUserStore = create((set) => ({
-    user: null,
-    setUser: (userData) => set({user: userData}),
-    clearUser: () => set({user: null}),
-}))
+  user: {
+    id: 0,
+    name: "Demo User",
+    roleId: 3,       // 관리자 권한 (화면 전부 접근 가능)
+    dept: "IT팀",
+    position: "Manager",
+  },
+  setUser: (userData) => set({ user: userData }),
+  clearUser: () => set({ user: null }),
+}));
 
 export default useUserStore;
